e75556d12ab907b6146d921b835bd7ecf4509ae7
DianaQuispe <danaalienmas@gmail.com>
2017-09-07 15:13:04 -0500

    estilos añadidos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 /*aquí va tu código*/
 function save() {
     var name = document.getElementById('name').value;
-    var coment = document.getElementById('coment').value;
-    sessionStorage.setItem(name, coment);
+    var clave = document.getElementById('clave').value;
+    sessionStorage.setItem(name, clave);
 
+}
+
+function recoverData() {
+    for (var I = 0; i < sessionStorage.length; i++) {
+        var name = sessionStorage.key(i);
+        var clave = sessionStorage.getItem(name);
+        document.getElementById('data').innerHTML =
+            var div = document.createElement('div');
+        var text = '<h1><b>' + name + '</b><br><p>' + clave + '</p></h1>';
+        div.appendChild(text);
+        data.appendChild
+    }
 }
