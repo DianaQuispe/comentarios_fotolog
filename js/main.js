9a2f89d4a85f34efc4d4589e20de652596bbc6ed
DianaQuispe <danaalienmas@gmail.com>
2017-09-07 15:04:14 -0500

    botones añadidos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 /*aquí va tu código*/
+function save() {
+    var name = document.getElementById('name').value;
+    var coment = document.getElementById('coment').value;
+    sessionStorage.setItem(name, coment);
+
+}
