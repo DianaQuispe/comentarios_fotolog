a00fd61c2fcc7649ad76030806fadccf5002f733
DianaQuispe <danaalienmas@gmail.com>
2017-09-08 10:36:09 -0500

    completando css

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
 /*aquí va tu código*/
 recoverData();
-function save() {
-    var name  = document.getElementById('name').value;
-    var clave  = document.getElementById('clave').value;
-    localStorage.setItem(name,clave);
-    //    localStorage.name = document.getElementById('name').value;
-    //    localStorage.clave = document.getElementById('clave').value;
-    //    localStorage.data = document.getElementById('divs');
-    //    //localStorage.nombrecito = document.getElementById('nombrecito').value;
 
+function save() {
+    var name = document.getElementById('name').value;
+    var clave = document.getElementById('clave').value;
+    localStorage.setItem(name, clave);
 }
 
 function recoverData() {
     document.getElementById('data').innerHTML = "";
-    for(var i =0; i<localStorage.length; i++) {
+    for (var i = 0; i < localStorage.length; i++) {
 
         var name = localStorage.key(i);
         var clave = localStorage.getItem(name);
@@ -21,11 +17,11 @@ function recoverData() {
         divs.setAttribute('id', 'divs');
         divs.className = 'color';
 
-        var  nombre = document.createElement('h3');
+        var nombre = document.createElement('h3');
         var texto1 = document.createTextNode(name);
         nombre.appendChild(texto1);
 
-        var comentario  = document.createElement('p');
+        var comentario = document.createElement('p');
         var texto2 = document.createTextNode(clave);
         comentario.appendChild(texto2);
 
@@ -33,23 +29,16 @@ function recoverData() {
         divs.appendChild(comentario);
         document.getElementById('data').appendChild(divs);
     }
-    //    var nombrecito = localStorage.getItem('nombre');
-    //    var clavecita = localStorage.getItem('clave');
-    //
-    //    if((nombrecito != undefined) &&(clavecita != undefined)) {
-    //
-    //    }
 
 }
-function coment()  {
-    if (typeof(Storage) !== "undefined") {
+
+function coment() {
+    if (typeof (Storage) !== "undefined") {
         save();
         recoverData();
         document.getElementById("name").value = "";
         document.getElementById("clave").value = "";
-        // Store
-        // localStorage.setItem("lastname", "Smith");
-        // Retrieve
+
     } else {
         document.getElementById("data").innerHTML = "Sorry, your browser does not support Web Storage...";
     }
@@ -57,6 +46,7 @@ function coment()  {
 
 
 }
+
 function cleanData() {
     document.getElementById("data").innerHTML = "";
 
